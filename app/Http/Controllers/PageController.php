bab1a239557479ab7bedd58cf5ea2abe13ca2ceb
Michael <roscualmichael@gmail.com>
2026-09-01 14:53:23 +0800

    bungkag na HAHAHAH

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,29 +10,4 @@ class PageController extends Controller
     {
         return view('admin.home');
     }
-
-    public function userHome()
-    {
-        return view('user.home');
-    }
-
-    public function matches()
-    {
-        return view('user.matches');
-    }
-
-    public function exchanges()
-    {
-        return view('user.exchanges');
-    }
-
-    public function messages()
-    {
-        return view('user.messages');
-    }
-
-    public function profile()
-    {
-        return view('user.profile');
-    }
 }
